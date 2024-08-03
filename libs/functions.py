075aa486cf866a0531db37f0196ff8d86bea5411
Alexander Snyder <alexander@thisguyshouldworkfor.us>
2024-08-02 19:57:43 -0700

    refactor: Update LogIt function to notify only on error or critical log levels

--- libs/functions.py
+++ libs/functions.py
@@ -83,7 +83,7 @@ def LogIt(LOG: str = None, message: str = None, LEVEL: str = 'info'):
         logger.log(log_level, message)
 
         # Notify via NotifyMe if the level is 'error' or higher
-        if log_level >= logging.ERROR:
+        if ((log_level == logging.ERROR) or (log_level == logging.CRITICAL)):
             NotifyMe(title=f"{LEVEL.upper()} Notification", priority=5, tags='face_with_spiral_eyes', message=message)
 
     return logger
